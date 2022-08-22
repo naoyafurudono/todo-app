926212b79626113308f986222196573480067671
naoyafurudono <naoyafurudono@prg.is.titech.ac.jp>
2022-08-22 23:21:07 +0900

    websocket 通信の実現

--- client/todo-app/src/components/TodoApp.tsx
+++ client/todo-app/src/components/TodoApp.tsx
@@ -1,11 +1,44 @@
-import React, { useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import Controller, { FilterCond } from "./Controller";
 import Input from "./Input";
 import TodoList, { itemsSample } from "./TodoList";
 import { Item, createItem, ID } from './TodoItem';
 
 
-const TodoApp: React.FC<{ space: string }> = ({space}) => {
+const TodoApp: React.FC<{ space: string }> = ({ space }) => {
+    // set up ws
+    const clientRef = useRef<WebSocket>()
+    const [msgs, setMsgs] = useState<string[]>([])
+
+    useEffect(() => {
+        const wsClient = new WebSocket("ws://localhost:8080/ws")
+        clientRef.current = wsClient;
+        wsClient.onopen = () => {
+            console.log("connect");
+            wsClient.send('hello from client')
+        };
+        wsClient.onclose = () => console.log("closed")
+
+
+        return () => { wsClient.close() };
+    }, []);
+
+    useEffect(() => {
+        if (!clientRef.current) {
+            return;
+        }
+        const ws = clientRef.current;
+        ws.onmessage = ((event: any) => {
+            console.log("message come")
+            console.log(event.data)
+            const newMsgs = msgs.slice();
+            const msg = event.Data;
+            newMsgs.push(msg);
+            setMsgs(newMsgs);
+            ws.send(JSON.stringify('how are you?'))
+        });
+
+    }, [msgs])
 
     const [items, setItems] = useState<Item[]>(itemsSample);
     const [show, setShow] = useState<FilterCond>('all');
@@ -39,6 +72,7 @@ const TodoApp: React.FC<{ space: string }> = ({space}) => {
 
     return (
         <div>
+
             <header>
                 space: {space}
             </header>
